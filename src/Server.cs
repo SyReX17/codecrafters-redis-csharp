7403470c60f93e7a8ac8b32afb2bd7de578f3165
SyReX17 <aaskarov@addcode.ru>
2023-01-27 10:31:14 +0300

    test version

--- src/Server.cs
+++ src/Server.cs
@@ -5,20 +5,23 @@ using System.Net.Sockets;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 using var socket = server.AcceptTcpClient();
-using var stream = socket.GetStream();
-using var reader = new StreamReader(stream);
-using var writer = new StreamWriter(stream);
-writer.AutoFlush = true;
-
+Console.WriteLine(socket.Connected);
+var stream = socket.GetStream();
 try
 {
-    var inputLine = reader.ReadLine();
-    while (!string.IsNullOrEmpty(inputLine))
+    using var reader = new StreamReader(stream);
+    using var writer = new StreamWriter(stream);
+    writer.AutoFlush = true;
+    string line;
+    Console.WriteLine("Start read");
+    while((line = reader.ReadLine()) != null) 
     {
-        if (inputLine == "ping")
+        Console.WriteLine("test");
+        Console.WriteLine(line);
+        if (line == "ping")
         {
             writer.Write("+PONG\r\n");
             writer.Flush();
