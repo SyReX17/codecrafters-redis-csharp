4f04ea6c4d7ff24a1f4518de496195b473ea4201
Askarov Artem <aaskarov@addcode.ru>
2023-03-07 10:40:46 +0300

    Try add multiple pings

--- src/Server.cs
+++ src/Server.cs
@@ -8,24 +8,18 @@ Console.WriteLine("Logs from your program will appear here!");
 var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 using var socket = server.AcceptTcpClient();
-Console.WriteLine(socket.Connected);
 var stream = socket.GetStream();
 try
 {
     using var reader = new StreamReader(stream);
     using var writer = new StreamWriter(stream);
     writer.AutoFlush = true;
-    string line;
-    Console.WriteLine("Start read");
-    while((line = reader.ReadLine()) != null) 
+    while(reader.ReadLine() is {} line) 
     {
-        Console.WriteLine("test");
-        Console.WriteLine(line);
         if (line == "ping")
         {
             writer.Write("+PONG\r\n");
             writer.Flush();
-            break;
         }
     }
 }
