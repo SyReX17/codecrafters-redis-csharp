dbf8c8cb6783c6923b5f8d2accaa112fd2a010e2
Askarov Artem <aaskarov@addcode.ru>
2023-03-09 11:13:28 +0300

    add logging

--- src/CommandsExtensions.cs
+++ src/CommandsExtensions.cs
@@ -6,23 +6,29 @@ public static class CommandsExtensions
     {
         var i = 0;
 
+        Console.WriteLine("StartHandle...");
+
         while (i < args.Length)
         {
             var output = "";
             if (args[i] == "*1" && args[i + 2].ToLower().Contains("ping"))
             {
+                Console.WriteLine("ping command");
                 output = "+PONG\r\n";
             }
             else if (args[i] == "*1" && args[i + 2].ToLower().Contains("echo"))
             {
+                Console.WriteLine("echo command");
                 output = $"+{args[i + 4]}\r\n";
             }
 
+            Console.WriteLine("start response...");
             if (!string.IsNullOrEmpty(output))
             {
                 sw.Write(output);
                 sw.Flush();
             }
+            Console.WriteLine("move next...");
             i += 2;
         }
     }
